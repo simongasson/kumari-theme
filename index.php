980f2d48147f76c0352112c1b0fa6d680a1f4d59
Simon <simongasson@gmail.com>
2019-02-19 09:34:32 +0200

    Work on home page

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 
     <div>
         <div class="row grey-block">
-            <img class="col-s-12 col-m-6" src="<?php echo get_bloginfo('template_url') ?>/images/landing-page.jpg" />
+            <div class="col-s-12 col-m-6 square-block beach">
+                <img src="<?php echo get_bloginfo('template_url') ?>/images/landing-page.jpg" />
+            </div>
             <div class="col-s-12 col-m-6">
-                <div class="center-block">
+                <div class="center-block square-block">
                     <p>
                         <h2>Kumari</h2>
                         Fun Wear. <br /> <br /> KUMARI is a fun fashion clothing range, catering for all ages who enjoy colour and curves. Our joyful range is suitable for all occasions including active or everyday wear by feeling free and positive. We strive to bring style and orders to all parts of the world, enabling our customes to enjoy our happy fun range from wherever you are. From design to concept, and straight to your doorstep.
